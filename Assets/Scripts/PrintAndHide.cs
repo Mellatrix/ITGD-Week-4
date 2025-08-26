581eea8ad59757fdf2f28ee2e27c305c55967b98
Mellatrix <89572289+Mellatrix@users.noreply.github.com>
2025-08-27 00:31:00 +1000

    hide-feature

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class PrintAndHide : MonoBehaviour
 {
     private int i;
+    private int randVal;
     public Renderer rend;
     
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        randVal = Random.Range(150, 251);
     }
 
     // Update is called once per frame
@@ -18,5 +20,15 @@ public class PrintAndHide : MonoBehaviour
     {
         i++;
         Debug.Log(gameObject.name + ":" + i);
+        if (gameObject.CompareTag("Red") && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (gameObject.CompareTag("Blue") && i == randVal)
+        {
+            gameObject.SetActive(false);
+        }
+        
     }
 }
